617095ad05e190e2026740b316c602c8a1627517
Zama Phungula <102163471+Zamap708@users.noreply.github.com>
2025-03-06 13:16:59 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // GitHub Pages does not support Image Optimization
+  },
+  // basePath: "/AAA", // Must match your repo name
+  // assetPrefix: "/",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
